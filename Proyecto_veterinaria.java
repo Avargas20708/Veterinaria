8d31a5ae42735c45169906b15d44098401b4b2cb
ReychellMendez2005 <mreychell00904@ufide.ac.cr>
2026-03-12 15:25:29 -0600

    Java
    Se agrega el código fuente en Java del sistema de gestión de la clínica veterinaria, incluyendo la conexión a la base de datos y las clases principales del sistema.

---
+++ Proyecto_veterinaria.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyecto_veterinaria;
+
+/**
+ *
+ * @author Reychell
+ */
+public class Proyecto_veterinaria {
+
+    public static void main(String[] args) {
+
+        ClienteDAO.insertarCliente(
+                1,
+                "Carlos",
+                "Perez",
+                "8888"
+        );
+
+    }
+}
